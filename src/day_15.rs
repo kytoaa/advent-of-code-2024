e858f6d4e3004c38f7c121b93b7bba75acf49b57
kytoaa <58483576+kytoaa@users.noreply.github.com>
2024-12-17 08:37:55 +0000

    day 15 part 2

--- src/day_15.rs
+++ src/day_15.rs
@@ -10,11 +10,20 @@ pub fn run(mut dir: String) {
 fn solve(data: &str) -> usize {
     let data = data.trim();
 
-    let map: String = data
-        .split_inclusive('\n')
-        .take_while(|line| !line.trim().is_empty())
-        .collect();
+    let map: String = {
+        let mut map = String::new();
+        let mut lines = data
+            .lines()
+            .take_while(|line| !line.trim().is_empty())
+            .map(to_wide_map);
+        while let Some(s) = lines.next() {
+            map.push_str(&s);
+            map.push('\n');
+        }
+        map
+    };
     let mut map = parse_map(&map);
+    let box_count = map.box_count();
 
     let instructions: String = data
         .split_inclusive('\n')
@@ -22,20 +31,28 @@ fn solve(data: &str) -> usize {
         .collect();
     let mut instructions = parse_instructions(&instructions);
 
-    while let Some(instruction) = instructions.next() {
-        map.move_robot(instruction);
+    let step = 1;
+    'outer: loop {
+        for _ in 0..step {
+            if let Some(instruction) = instructions.next() {
+                map.move_robot(instruction);
+            } else {
+                break 'outer;
+            }
+        }
     }
 
+    assert_eq!(map.box_count(), box_count);
     map.gps_sum()
 }
 
 #[derive(PartialEq, Clone, Copy, Debug)]
 enum Space {
     Wall,
-    Box,
+    Box(char),
     Empty,
 }
-#[derive(Clone, Copy)]
+#[derive(PartialEq, Clone, Copy, Debug)]
 enum Instruction {
     Left,
     Right,
@@ -43,7 +60,7 @@ enum Instruction {
     Down,
 }
 
-#[derive(Debug)]
+#[derive(PartialEq, Debug)]
 struct Map {
     map: Vec<Vec<Space>>,
     robot_pos: (usize, usize),
@@ -54,12 +71,18 @@ impl Map {
             .iter()
             .enumerate()
             .map(|(y, line)| {
-                line.iter().enumerate().map(move |(x, c)| match c {
-                    Space::Box => y * 100 + x,
-                    _ => 0,
-                })
+                line.iter()
+                    .enumerate()
+                    .filter(|(_, c)| **c == Space::Box('['))
+                    .map(move |(x, _)| y * 100 + x)
             })
             .flatten()
+            .sum()
+    }
+    fn box_count(&self) -> usize {
+        self.map
+            .iter()
+            .map(|line| line.iter().filter(|c| **c == Space::Box('[')).count())
             .sum()
     }
     fn move_robot(&mut self, dir: Instruction) {
@@ -69,16 +92,28 @@ impl Map {
             Instruction::Left => (self.robot_pos.0 - 1, self.robot_pos.1),
             Instruction::Right => (self.robot_pos.0 + 1, self.robot_pos.1),
         };
-        if self.push(new_pos, dir) {
+        let check = self.push_and_check(new_pos, dir, &mut vec![]);
+        if let Some(f) = check {
+            f(self, dir);
             self.robot_pos = new_pos;
         }
     }
-    fn push(&mut self, pos: (usize, usize), dir: Instruction) -> bool {
+    fn push_and_check(
+        &mut self,
+        pos: (usize, usize),
+        dir: Instruction,
+        checked: &mut Vec<(usize, usize)>,
+    ) -> Option<Box<dyn FnOnce(&mut Self, Instruction)>> {
+        if checked.contains(&pos) {
+            return Some(Box::new(|_, _| ()));
+        }
         match self.map.get(pos.1).unwrap().get(pos.0).unwrap() {
-            Space::Empty => true,
-            Space::Wall => false,
-            Space::Box => {
-                if self.push(
+            Space::Empty => Some(Box::new(|_, _| ())),
+            Space::Wall => None,
+            Space::Box(side) => {
+                let side = *side;
+                checked.push(pos.clone());
+                let front_check = self.push_and_check(
                     match dir {
                         Instruction::Up => (pos.0, pos.1 - 1),
                         Instruction::Down => (pos.0, pos.1 + 1),
@@ -86,34 +121,88 @@ impl Map {
                         Instruction::Right => (pos.0 + 1, pos.1),
                     },
                     dir,
-                ) {
-                    *self.map.get_mut(pos.1).unwrap().get_mut(pos.0).unwrap() = Space::Empty;
+                    checked,
+                );
+                if front_check.is_none() {
+                    return None;
+                }
+                let side_check = match dir {
+                    Instruction::Up | Instruction::Down => {
+                        let side_check = self.push_and_check(
+                            {
+                                let offset = match side {
+                                    '[' => 1,
+                                    ']' => -1,
+                                    c => panic!("{c} is not a box side"),
+                                };
+                                ((pos.0 as isize + offset) as usize, pos.1)
+                            },
+                            dir,
+                            checked,
+                        );
+                        if side_check.is_none() {
+                            return None;
+                        }
+                        side_check
+                    }
+                    _ => None,
+                };
+                Some(Box::new(move |s, dir| {
+                    if let Some(f) = front_check {
+                        f(s, dir);
+                    }
+                    if let Some(f) = side_check {
+                        f(s, dir);
+                    }
+                    let moved = *s.map.get(pos.1).unwrap().get(pos.0).unwrap();
+                    *s.map.get_mut(pos.1).unwrap().get_mut(pos.0).unwrap() = Space::Empty;
 
                     match dir {
                         Instruction::Up => {
-                            *self.map.get_mut(pos.1 - 1).unwrap().get_mut(pos.0).unwrap() =
-                                Space::Box
+                            *s.map.get_mut(pos.1 - 1).unwrap().get_mut(pos.0).unwrap() = moved;
                         }
                         Instruction::Down => {
-                            *self.map.get_mut(pos.1 + 1).unwrap().get_mut(pos.0).unwrap() =
-                                Space::Box
+                            *s.map.get_mut(pos.1 + 1).unwrap().get_mut(pos.0).unwrap() = moved;
                         }
                         Instruction::Left => {
-                            *self.map.get_mut(pos.1).unwrap().get_mut(pos.0 - 1).unwrap() =
-                                Space::Box
+                            *s.map.get_mut(pos.1).unwrap().get_mut(pos.0 - 1).unwrap() = moved;
                         }
                         Instruction::Right => {
-                            *self.map.get_mut(pos.1).unwrap().get_mut(pos.0 + 1).unwrap() =
-                                Space::Box
+                            *s.map.get_mut(pos.1).unwrap().get_mut(pos.0 + 1).unwrap() = moved
                         }
                     }
-                    true
-                } else {
-                    false
-                }
+                }))
             }
         }
     }
+    #[allow(dead_code)]
+    fn display(&self) -> String {
+        let mut string = String::new();
+
+        for (y, line) in self.map.iter().enumerate() {
+            for (x, space) in line.iter().enumerate() {
+                if self.robot_pos == (x, y) {
+                    string.push('@');
+                    continue;
+                }
+                string.push(match space {
+                    Space::Wall => '#',
+                    Space::Box(c) => *c,
+                    Space::Empty => '.',
+                });
+            }
+            string.push('\n');
+        }
+
+        string
+    }
+}
+
+fn to_wide_map(map: &str) -> String {
+    map.replace("#", "##")
+        .replace("O", "[]")
+        .replace(".", "..")
+        .replace("@", "@.")
 }
 
 fn parse_map(map: &str) -> Map {
@@ -132,7 +221,8 @@ fn parse_map(map: &str) -> Map {
                     }
                     match c {
                         '#' => Space::Wall,
-                        'O' => Space::Box,
+                        '[' => Space::Box('['),
+                        ']' => Space::Box(']'),
                         '.' | '@' => Space::Empty,
                         c => panic!("{c} is not a tile"),
                     }
@@ -166,76 +256,263 @@ mod tests {
     use super::*;
 
     #[test]
-    fn push_test() {
-        let mut map = Map {
-            map: vec![
-                vec![Space::Wall],
-                vec![Space::Empty],
-                vec![Space::Box],
-                vec![Space::Box],
-                vec![Space::Empty],
-            ],
-            robot_pos: (0, 4),
-        };
-
-        map.move_robot(Instruction::Up);
-        assert_eq!(
-            map.map,
-            vec![
-                vec![Space::Wall],
-                vec![Space::Box],
-                vec![Space::Box],
-                vec![Space::Empty],
-                vec![Space::Empty],
-            ]
-        );
-
-        map.move_robot(Instruction::Up);
-        assert_eq!(
-            map.map,
-            vec![
-                vec![Space::Wall],
-                vec![Space::Box],
-                vec![Space::Box],
-                vec![Space::Empty],
-                vec![Space::Empty],
-            ]
-        );
-    }
-
-    #[test]
     fn gps_sum_test() {
         let map = parse_map(
-            "#######
-#...O..
-#......",
+            "
+##########
+##...[]...
+##........",
         );
-        println!("{:?}", map);
 
-        assert_eq!(map.gps_sum(), 104);
+        assert_eq!(map.gps_sum(), 105);
     }
 
     #[test]
-    fn small_example_test() {
-        let example = "
-########
-#..O.O.#
-##@.O..#
-#...O..#
-#.#.O..#
-#...O..#
-#......#
-########
+    fn push_test() {
+        let mut map = parse_map(
+            "..#
+...
+.[]
+[].
+@..",
+        );
 
-<^^>>>vv<v>>v<<";
+        map.move_robot(Instruction::Up);
+        assert_eq!(
+            map,
+            parse_map(
+                "..#
+.[]
+[].
+@..
+...",
+            )
+        );
 
-        assert_eq!(solve(example), 2028);
+        map.move_robot(Instruction::Up);
+        assert_eq!(
+            map,
+            parse_map(
+                "..#
+.[]
+[].
+@..
+...",
+            )
+        );
+    }
+    #[test]
+    fn complex_push_tests() {
+        let mut map = parse_map(
+            "
+...#
+[]..
+..[]
+.[].
+.@.."
+                .trim(),
+        );
+
+        map.move_robot(Instruction::Up);
+        assert_eq!(
+            map,
+            parse_map(
+                "
+...#
+[][]
+.[].
+.@..
+...."
+                    .trim(),
+            )
+        );
+
+        map.move_robot(Instruction::Up);
+        assert_eq!(
+            map,
+            parse_map(
+                "
+...#
+[][]
+.[].
+.@..
+...."
+                    .trim(),
+            )
+        );
+
+        let mut map = parse_map(
+            "
+....
+.[]#
+[]..
+..[]
+.[].
+.@.."
+                .trim(),
+        );
+
+        map.move_robot(Instruction::Up);
+        assert_eq!(
+            map,
+            parse_map(
+                "
+....
+.[]#
+[][]
+.[].
+.@..
+...."
+                    .trim(),
+            )
+        );
+
+        map.move_robot(Instruction::Up);
+        assert_eq!(
+            map,
+            parse_map(
+                "
+....
+.[]#
+[][]
+.[].
+.@..
+...."
+                    .trim(),
+            )
+        );
+
+        let mut map = parse_map(
+            "
+....
+...#
+.[].
+[][]
+.[].
+.@.."
+                .trim(),
+        );
+
+        map.move_robot(Instruction::Up);
+        assert_eq!(
+            map,
+            parse_map(
+                "
+....
+.[]#
+[][]
+.[].
+.@..
+...."
+                    .trim(),
+            )
+        );
+
+        map.move_robot(Instruction::Up);
+        assert_eq!(
+            map,
+            parse_map(
+                "
+....
+.[]#
+[][]
+.[].
+.@..
+...."
+                    .trim(),
+            )
+        );
+
+        let mut map = parse_map(
+            "
+....
+...#
+.[].
+..[]
+.[].
+.[].
+.@.."
+                .trim(),
+        );
+
+        map.move_robot(Instruction::Up);
+        assert_eq!(
+            map,
+            parse_map(
+                "
+....
+.[]#
+..[]
+.[].
+.[].
+.@..
+...."
+                    .trim(),
+            )
+        );
+
+        map.move_robot(Instruction::Up);
+        assert_eq!(
+            map,
+            parse_map(
+                "
+....
+.[]#
+..[]
+.[].
+.[].
+.@..
+...."
+                    .trim(),
+            )
+        );
+
+        let mut map = parse_map(
+            "
+......
+..#..#
+......
+[][][]
+.[][].
+..[]..
+..@..."
+                .trim(),
+        );
+
+        map.move_robot(Instruction::Up);
+        assert_eq!(
+            map,
+            parse_map(
+                "
+......
+..#..#
+[][][]
+.[][].
+..[]..
+..@...
+......"
+            )
+        );
+
+        map.move_robot(Instruction::Up);
+        assert_eq!(
+            map,
+            parse_map(
+                "
+......
+..#..#
+[][][]
+.[][].
+..[]..
+..@...
+......"
+            )
+        );
     }
 
     #[test]
     fn example_test() {
-        let example = "
-##########
+        let example = "##########
 #..O..O.O#
 #......O.#
 #.OO..O.O#
@@ -257,6 +534,6 @@ vvv<<^>^v^^><<>>><>^<<><^vv^^<>vvv<>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v
 ^^>vv<^v^v<vv>^<><v<^v>^^^>>>^^vvv^>vvv<>>>^<^>>>>>^<<^v>^vvv<>^<><<v>
 v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^";
 
-        assert_eq!(solve(example), 10092);
+        assert_eq!(solve(example), 9021);
     }
 }
